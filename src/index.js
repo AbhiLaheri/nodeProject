1491c43f0317aa27f81882e274d418b09f830281
ankita <ankitabasak18@gmail.com>
2019-02-20 12:40:13 +0530

    New File

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Routerr from './class8/router';
+
+//import Routerr from './class8/router';
 // import Delete from './practice/delete';
 //import Box from './box';
 //import Login from './login';
 //import Fetcher2 from './class6/fetcher2';
-import BackGraund from './class6/backGraund';
+import Admin from './admin';
+//import Emp1 from './Emp1';
+//import BackGraund from './class6/backGraund';
 import * as serviceWorker from './serviceWorker';
 
 //ReactDOM.render(<Login/>, document.getElementById('root'));
-ReactDOM.render(<Routerr/>, document.getElementById('root'));
-
+//ReactDOM.render(<Routerr/>, document.getElementById('root'));
+ReactDOM.render(<Admin/>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
