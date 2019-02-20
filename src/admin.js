c402e5842b352ebb86dd2b0ad522be0f0556fc94
ankita <ankitabasak18@gmail.com>
2019-02-20 18:20:18 +0530

    Modified Amin Page

--- src/admin.js
+++ src/admin.js
@@ -13,24 +13,29 @@ export default class Admin extends Component {
         return (
 
 <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">HOME</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      <NavDropdown title="Allotment For Query" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Executive 1</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Executive 2</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Executive 3</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Executive 4</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Executive 5</NavDropdown.Item>
+      </NavDropdown>
+      <NavDropdown title="Query For :" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Flipkart</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Amazon</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Myntra</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Shein</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Club Factory</NavDropdown.Item>
       </NavDropdown>
     </Nav>
     <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
+      <Button variant="outline-success">LogOut</Button>
     </Form>
+
   </Navbar.Collapse>
 </Navbar>
         )
